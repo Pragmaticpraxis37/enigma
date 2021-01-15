409a0670af98e25e87cb1497765c88e1ca1107c4
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 15:16:41 -0700

    Create initialize method for Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,14 @@
 class Enigma
 
-end 
+  attr_reader :message,
+              :key,
+              :date
+
+  def initialize(message, date = Time.now, *key)
+    @message = message
+    @key = key[0]
+    require "pry"; binding.pry
+    @date = date
+  end
+
+end
