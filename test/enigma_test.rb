cddaa269726e4e0640fee0b225bb16c1af90c13e
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 15:17:30 -0700

    Modify test for test_it_exists_and_has_attributes with test passing

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,11 +3,11 @@ require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    enigma = Enigma.new("Hello world", "10101", "040800")
+    enigma = Enigma.new("Hello world", "040800", "10101")
 
     assert_instance_of Enigma, enigma
     assert_equal "Hello world", enigma.message
-    assert_equal 10101, enigma.key
-    assert_equal 40800, enigma.date
+    assert_equal "10101", enigma.key
+    assert_equal "040800", enigma.date
   end
 end
