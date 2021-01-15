c2cdda25cfa7a6bf60c418f16ba66eda07ed7486
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 14:19:55 -0700

    Create test directory

---
+++ test/enigma_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper' 
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+end
