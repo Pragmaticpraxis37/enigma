3c192fd9f0589e130c3a13b9e8c2472650d7c6a6
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 14:40:22 -0700

    Create test_it_exists_and_has_attributes for Enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,10 +1,13 @@
-require './test/test_helper' 
+require './test/test_helper'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    enigma = Enigma.new
+    enigma = Enigma.new("Hello world", "10101", "040800")
 
     assert_instance_of Enigma, enigma
+    assert_equal "Hello world", enigma.message
+    assert_equal 10101, enigma.key
+    assert_equal 40800, enigma.date
   end
 end
