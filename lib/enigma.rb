f33a6b5b85d13a7c383b75b8a7d151c4df6dccf8
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 15:45:52 -0700

    Modify class Enigma initialize to take no arguments

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,57 +1,73 @@
+require 'date'
+require './lib/key'
+
 class Enigma
 
-  attr_reader :message,
-              :key,
-              :date
+  def initialize
 
-  def initialize(message, *optional_key_and_date)
-    @message = message
-    @key = determine_key(optional_key_and_date)
-    @date = determine_date(optional_key_and_date)
   end
 
-  def determine_key(optional_key_and_date)
-    if optional_key_and_date.empty?
-      return create_key
-    elsif optional_key_and_date.length == 1 && optional_key_and_date[0].length != 5
-      return create_key
-    else
-      return optional_key_and_date[0]
-    end
+  def encrypt(message, key='none', date='none')
+    key = Key.new(key)
+    require "pry"; binding.pry
   end
 
-  def create_key
-    key = rand(99999)
-    key_as_string = key.to_s
-    if key_as_string.length == 5
-      key_as_string
-    else
-      trailing_zeros = 5 - key_as_string
-      key_as_string.prepend("0" * trailing_zeros)
-    end
+  def decrypt(ciphertext, key, date='none')
+
   end
 
-  def determine_date(optional_key_and_date)
-    if optional_key_and_date.empty?
-      return create_date
-    elsif optional_key_and_date.length == 1 && optional_key_and_date[0].length != 6
-      return create_date
-    else
-      return optional_key_and_date[1]
-    end
-  end
-
-  def create_date
-    @date = Time.now.strftime("%d%m%y")
-  end
 end
 
+  # attr_reader :message,
+  #             :key,
+  #             :date
 
-# @key = optional_key_and_date[0]
-# require "pry"; binding.pry
-# if optional_key_and_date[1].nil? == true
-#   require "pry"; binding.pry
-#   @date = true
-# else
-#   @date = Time.now.strftime("%d%m%y")
-# end
+  # def initialize(message, date = "none", key = "none" *optional_key_and_date)
+  #   @message = message
+  #   @key = determine_key(optional_key_and_date)
+  #   @date = determine_date(optional_key_and_date)
+  #
+  #   user_date = date
+  #
+  #   if date == "none"
+  #     create date
+  #   else
+  #     date
+  #
+  # end
+
+  # "%05d" %(x)
+  # def determine_key(optional_key_and_date)
+  #   if optional_key_and_date.empty?
+  #     return create_key
+  #   elsif optional_key_and_date.length == 1 && optional_key_and_date[0].length != 5
+  #     return create_key
+  #   else
+  #     return optional_key_and_date[0]
+  #   end
+  # end
+  #
+  # def create_key
+  #   key = rand(99999)
+  #   key_as_string = key.to_s
+  #   if key_as_string.length == 5
+  #     key_as_string
+  #   else
+  #     trailing_zeros = 5 - key_as_string
+  #     key_as_string.prepend("0" * trailing_zeros)
+  #   end
+  # end
+  #
+  # def determine_date(optional_key_and_date)
+  #   if optional_key_and_date.empty?
+  #     return create_date
+  #   elsif optional_key_and_date.length == 1 && optional_key_and_date[0].length != 6
+  #     return create_date
+  #   else
+  #     return optional_key_and_date[1]
+  #   end
+  # end
+  #
+  # def create_date
+  #   @date = Time.now.strftime("%d%m%y")
+  # end
