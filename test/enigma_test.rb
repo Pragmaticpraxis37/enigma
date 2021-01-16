ef81287df76f28f8822af294f124389a76010277
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 15:46:26 -0700

    Modify tests for Enigma class now that initialize takes no arguments

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,41 +3,51 @@ require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists_and_has_attributes
-    enigma = Enigma.new("Hello world", "10101", "040800")
+    enigma = Enigma.new
 
     assert_instance_of Enigma, enigma
-    assert_equal "Hello world", enigma.message
-    assert_equal "10101", enigma.key
-    assert_equal "040800", enigma.date
   end
 
-  def test_date_argument_has_default_value
-    enigma = Enigma.new("Hello world", "10101")
+  def test_encrypt_method_creates_key_object
+    enigma = Enigma.new
 
-    assert_equal "Hello world", enigma.message
-    assert_equal "10101", enigma.key
-    assert_equal Time.now.strftime("%d%m%y"), enigma.date
+    enigma.encrypt('Hello world')
+    require "pry"; binding.pry
   end
-
-  def test_key_argument_has_default_value
-    enigma = Enigma.new("Hello world", "040800")
-    assert_equal 5, enigma.key.length
-  end
-
-  def test_key_is_generated_if_no_optional_arguments
-    enigma = Enigma.new("Hello")
-    assert_equal 5, enigma.key.length
-  end
-
-  def test_date_is_generated_if_no_optional_arguments
-    enigma = Enigma.new("Hello")
-    assert_equal Time.now.strftime("%d%m%y"), enigma.date
-  end
-
-  def test_date_and_key_arguments_have_default_values
-    enigma = Enigma.new("Hello world")
-    assert_equal "Hello world", enigma.message
-    assert_equal 5, enigma.key.length
-    assert_equal Time.now.strftime("%d%m%y"), enigma.date
-  end
+  #
+  # def test_date_argument_has_default_value
+  #   enigma = Enigma.new("Hello world", "10101")
+  #
+  #   assert_equal "Hello world", enigma.message
+  #   assert_equal "10101", enigma.key
+  #   assert_equal Time.now.strftime("%d%m%y"), enigma.date
+  # end
+  #
+  # def test_key_is_assigned_when_optional_argument_for_it_is_provided
+  #   enigma = Enigma.new("Hello world", "10101")
+  #
+  #   assert_equal "10101", enigma.key
+  # end
+  #
+  # def test_key_argument_has_default_value
+  #   enigma = Enigma.new("Hello world", "040800")
+  #   assert_equal 5, enigma.key.length
+  # end
+  #
+  # def test_key_is_generated_if_no_optional_arguments
+  #   enigma = Enigma.new("Hello")
+  #   assert_equal 5, enigma.key.length
+  # end
+  #
+  # def test_date_is_generated_if_no_optional_arguments
+  #   enigma = Enigma.new("Hello")
+  #   assert_equal Time.now.strftime("%d%m%y"), enigma.date
+  # end
+  #
+  # def test_date_and_key_arguments_have_default_values
+  #   enigma = Enigma.new("Hello world")
+  #   assert_equal "Hello world", enigma.message
+  #   assert_equal 5, enigma.key.length
+  #   assert_equal Time.now.strftime("%d%m%y"), enigma.date
+  # end
 end
