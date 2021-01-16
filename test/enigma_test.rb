04516b196eabb5df746e08af18bc99829adf4e3a
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 17:14:32 -0700

    Create key_argument_has_default_value test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,6 +14,17 @@ class EnigmaTest < Minitest::Test
   def test_date_argument_has_default_value
     enigma = Enigma.new("Hello world", "10101")
 
+    assert_equal "Hello world", enigma.message
+    assert_equal "10101", enigma.key
     assert_equal Time.now.strftime("%d%m%y"), enigma.date
+  end
+
+  def test_key_argument_has_default_value
+    enigma = Enigma.new("Hello world", "040800")
+
+    assert_instance_of Enigma, enigma
+    assert_equal "Hello world", enigma.message
+    assert_equal 5, enigma.key.length 
+    assert_equal "040800", enigma.date
   end
 end
