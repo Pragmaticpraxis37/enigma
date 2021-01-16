95f7ec0b3c753b7aef367f143fb5d3eb0e6c86a9
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 16:15:35 -0700

    Create test_determine_if_key_returns_key_provided_by_user

--- test/key_test.rb
+++ test/key_test.rb
@@ -7,6 +7,12 @@ class KeyTest < Minitest::Test
   def test_it_exists_and_has_attributes
     key = Key.new("00001")
 
-    assert_instance_of Key, key 
+    assert_instance_of Key, key
+  end
+
+  def test_determine_if_key_returns_key_provided
+    key = Key.new("00001")
+
+    assert_equal "00001", key.key 
   end
 end
