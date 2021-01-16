bb06edaf69f47a87004b4701405139d23822d1c8
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 16:17:52 -0700

    Create #determine_if_key_provided for class Enigma

--- lib/key.rb
+++ lib/key.rb
@@ -1,10 +1,19 @@
 class Key
+  attr_reader :key 
+
 
   def initialize(key)
-    @key = key.determine_key
+    @key = determine_if_key_provided(key)
   end
 
-  def determine_key
-    @key 
+  def determine_if_key_provided(key)
+    if key == 'none'
+      create_key
+    elsif (key.class == String) && key.length == 5
+      return key
+    else
+      "All user created keys must be a five digit string.  Please try again."
+    end
+
   end
 end
