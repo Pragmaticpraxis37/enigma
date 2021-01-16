b5a2dd86410849eff8a6aa0e0ca080189cf2dfe4
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 18:22:28 -0700

    Create #create_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,12 +6,60 @@ class Enigma
 
   def initialize(message, *optional_key_and_date)
     @message = message
-    @key = optional_key_and_date[0]
-    require "pry"; binding.pry
-    if optional_key_and_date[1].nil? == true
-      @date = create_date
+    @key = determine_key(optional_key_and_date)
+    # @date = determine_date(optional_key_and_date)
+  end
+
+  def determine_key(optional_key_and_date)
+    if optional_key_and_date.empty?
+      return create_key
+      require "pry"; binding.pry
+    # elsif optional_key_and_date.length == 1
+    #   determine_if_optional_argument_has_key
     else
-      @date = Time.now.strftime("%d%m%y")
+      find_key
+      require "pry"; binding.pry
     end
   end
+
+  def create_key
+    key = rand(99999)
+    require "pry"; binding.pry
+    key_as_string = key.to_s
+    require "pry"; binding.pry
+    if key_as_string.length == 5
+      require "pry"; binding.pry
+      key_as_string
+    else
+      trailing_zeros = 5 - key_as_string
+      require "pry"; binding.pry
+      key_as_string.prepend("0" * trailing_zeros)
+    end
+  end
+
+  def determine_if_optional_argument_has_key
+
+  end
+
+  def find_key
+
+  end
+
+
+
+  def determine_date(optional_key_and_date)
+    require "pry"; binding.pry
+  end
+
+
 end
+
+
+# @key = optional_key_and_date[0]
+# require "pry"; binding.pry
+# if optional_key_and_date[1].nil? == true
+#   require "pry"; binding.pry
+#   @date = true
+# else
+#   @date = Time.now.strftime("%d%m%y")
+# end
