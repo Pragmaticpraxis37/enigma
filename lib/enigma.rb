0e9cf1e4e2e233d70ba6cf392bd11410fffc530a
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-15 19:08:35 -0700

    Create #determine_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,26 +13,22 @@ class Enigma
   def determine_key(optional_key_and_date)
     if optional_key_and_date.empty?
       return create_key
+    elsif optional_key_and_date.length == 1 && optional_key_and_date[0].length != 5
       require "pry"; binding.pry
-    # elsif optional_key_and_date.length == 1
-    #   determine_if_optional_argument_has_key
+      return create_key
     else
-      find_key
       require "pry"; binding.pry
+      return optional_key_and_date[0]
     end
   end
 
   def create_key
     key = rand(99999)
-    require "pry"; binding.pry
     key_as_string = key.to_s
-    require "pry"; binding.pry
     if key_as_string.length == 5
-      require "pry"; binding.pry
       key_as_string
     else
       trailing_zeros = 5 - key_as_string
-      require "pry"; binding.pry
       key_as_string.prepend("0" * trailing_zeros)
     end
   end
