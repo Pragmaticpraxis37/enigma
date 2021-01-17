c46412a77b7d8c727ac36b7df7ccd4db8e3ef673
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 19:40:49 -0700

    Create test_create_date and test_determine_if_date_provided_returns_user_created_date

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -9,4 +9,17 @@ class OffsetTest < Minitest::Test
 
     assert_instance_of Offset, offset
   end
+
+  def test_create_key
+    skip
+    offset = Offset.new('none')
+
+    assert_equal 6, offset.date.length
+  end
+
+  def test_determine_if_key_provided_returns_user_created_key
+    key = Offset.new('010100')
+
+    assert_equal '010100', key.key
+  end
 end
