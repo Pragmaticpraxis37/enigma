b25180e03b45e946f3b07dba23bb65ba7495c877
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 20:25:20 -0700

    Create test_remove_last_four_of_squared_date

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -10,16 +10,29 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
   end
 
-  def test_create_key
-    skip
+  def test_create_date
     offset = Offset.new('none')
 
     assert_equal 6, offset.date.length
   end
 
-  def test_determine_if_key_provided_returns_user_created_key
-    key = Offset.new('010100')
+  def test_determine_if_date_provided_returns_user_created_date
+    offset = Offset.new('010100')
 
-    assert_equal '010100', key.key
+    assert_equal '010100', offset.date
+  end
+
+  def test_square_date
+    offset = Offset.new('010200')
+    offset.square_date('010200')
+
+    assert_equal 104040000, offset.squared("010200")
+  end
+
+  def test_remove_last_four_of_squared_date
+    skip
+    offset = Offset.new("010200")
+
+    assert_equal 0000, offset.square_date("010200")
   end
 end
