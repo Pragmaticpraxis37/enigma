7dde195bd4921c163e10f4dbeacc40c3da596762
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-17 15:55:28 -0700

    Create #create_sub_keys for Cipher class

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -9,8 +9,14 @@ class Cipher
     @offset = offset.split("")
   end
 
-  def method_name
+  def create_sub_keys
+    ord_letter = 97
 
+    @key.each_cons(2).reduce({}) do |sub_keys, cons_nums|
+      sub_keys[ord_letter.chr] = cons_nums.join.to_i
+      ord_letter += 1
+      sub_keys
+    end
   end
 
 end
