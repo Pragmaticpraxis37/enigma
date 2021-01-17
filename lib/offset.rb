391704d3b8120a0a44ea3e5e2ff5506aac00b6ce
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-17 08:14:50 -0700

    Create #split_offset_by_shift

--- lib/offset.rb
+++ lib/offset.rb
@@ -35,11 +35,19 @@ class Offset
 
   def square_date(date)
     @squared = date.to_i ** 2
-    remove_last_four_of_date_squared(squared)
+    remove_last_four_of_date_squared
   end
 
-  def remove_last_four_of_date_squared(squared)
+  def remove_last_four_of_date_squared
     @squared_last_four = @squared.to_s[-4..-1]
+    split_offset_by_shift
+  end
+
+  def split_offset_by_shift
+    @offset_shift_a = @squared_last_four[0]
+    @offset_shift_b = @squared_last_four[1]
+    @offset_shift_c = @squared_last_four[2]
+    @offset_shift_d = @squared_last_four[3]
   end
 
 end
