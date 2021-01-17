2ed3d6800c7099e83cceb568907b8786b6d8b680
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-16 20:26:40 -0700

    Create #remove_last_four_of_squared_date

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,13 +1,15 @@
 class Offset
 
-  attr_reader :date
+  attr_reader :date,
+              :squared
 
   def initialize(date)
-    @date = determine_if_date_provided(date)
+    @squared = 0
     @offset_shift_a = ""
     @offset_shift_b = ""
     @offset_shift_c = ""
     @offset_shift_d = ""
+    @date = determine_if_date_provided(date)
   end
 
   def determine_if_date_provided(date)
@@ -20,6 +22,16 @@ class Offset
 
   def create_date
     @date = Time.now.strftime("%d%m%y")
+  end
+
+  def square_date(date)
+    require "pry"; binding.pry
+    @squared = date.to_i ** 2
+    remove_last_four_of_squared_date(squared)
+  end
+
+  def remove_last_four_of_squared_date(squared)
+    @squared = squared[-4..-1]
     require "pry"; binding.pry
   end
 
