87f982cfd6f22c88527e4d7c3691be872e69704d
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-17 20:50:43 -0700

    Create test it_has_attributes for Decipher class

--- test/decipher_test.rb
+++ test/decipher_test.rb
@@ -6,6 +6,16 @@ require './lib/cipher'
 
 class DecipherTest < Minitest::Test
   def test_it_exists
-    decipher = Deipher.new("Hello world", "02715", "040895")
+    decipher = Deipher.new("keder ohulw", "02715", "1025")
     assert_instance_of Decipher, decipher
   end
+
+  def test_it_has_attributes
+    decipher = Decipher.new("keder ohulw", "02715", "1025")
+
+    assert_equal ["k", "e", "d", "e", "r", " ", "o", "h", "u", "l", "w"], decipher.message
+    assert_equal ["0", "2", "7", "1", "5"], decipher.key
+    assert_equal ["1", "0", "2", "5"], decipher.offset
+  end
+
+end
