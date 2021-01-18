9e3290c20d302a437bd582805a74d8b007379e65
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-18 08:23:36 -0700

    Remove methods used in Cipher and Decipher classes

--- lib/key.rb
+++ lib/key.rb
@@ -1,16 +1,8 @@
 class Key
-  attr_reader :key,
-              :key_shift_a,
-              :key_shift_b,
-              :key_shift_c,
-              :key_shift_d
+  attr_reader :key
 
   def initialize(key)
     @key = determine_if_key_provided(key)
-    @key_shift_a = ""
-    @key_shift_b = ""
-    @key_shift_c = ""
-    @key_shift_d = ""
     split_key_by_shift(key)
   end
 
@@ -26,12 +18,5 @@ class Key
     key = rand(1..99999)
     key = "%05d" %(key)
     return key
-  end
-
-  def split_key_by_shift(key)
-    @key_shift_a = @key[0..1]
-    @key_shift_b = @key[1..2]
-    @key_shift_c = @key[2..3]
-    @key_shift_d = @key[3..4]
   end
 end
