00b393217c920014ae06ceb58223e252e31ee8a4
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-18 10:23:18 -0700

    Create mock and stub to test #square_date and #remove_last_four_of_date_squared when date is created

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -7,22 +7,18 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
   end
 
-  def test_create_date
-    offset = Offset.new('none')
-
-    assert_equal 6, offset.date.length
-  end
-
   def test_determine_if_date_provided_returns_user_created_date
     offset = Offset.new('010100')
 
     assert_equal '010100', offset.date
   end
 
-  def test_square_date_when_user_created_date
-    offset = Offset.new('010200')
+  def test_create_date
+    offset = Offset.new('none')
 
-    assert_equal 104040000, offset.squared
+    assert_equal 6, offset.date.length
+    assert_equal String, offset.date.class
+    assert_equal Time.now.strftime("%d%m%y"), offset.date
   end
 
   def test_square_date_when_create_date_provides_date
@@ -31,33 +27,38 @@ class OffsetTest < Minitest::Test
     assert_equal 11, offset.squared.to_s.length
   end
 
+  def test_square_date_if_date_created
+    offset = Offset.new('none')
+    date = mock
+    date.stubs(:created_date).returns("180121")
+
+    assert_equal 32443574641, offset.squared
+  end
+
+
+  def test_square_date_when_user_created_date
+    offset = Offset.new('010200')
+
+    assert_equal 104040000, offset.squared
+  end
+
+
   def test_remove_last_four_of_date_squared_when_user_created_date
     offset = Offset.new('010200')
 
     assert_equal '0000', offset.squared_last_four
   end
 
-  def test_case_name_remove_last_four_of_date_squared_when_create_date_provides_date
+  def test_remove_last_four_of_date_squared_when_create_date_provides_date
     offset = Offset.new('none')
 
     assert_equal 4, offset.squared_last_four.length
-  end
 
-  def test_split_offset_by_shift_when_user_created_date
-    offset = Offset.new('010210')
+    date = mock
+    date.stubs(:created_date).returns("180121")
 
-    assert_equal '4', offset.offset_shift_a
-    assert_equal '1', offset.offset_shift_b
-    assert_equal '0', offset.offset_shift_c
-    assert_equal '0', offset.offset_shift_d
-  end
+    offset.square_date(date.created_date)
 
-  def test_split_offset_by_shift_when_create_date_provides_date
-    offset = Offset.new('none')
-
-    assert_equal 1, offset.offset_shift_a.length
-    assert_equal 1, offset.offset_shift_b.length
-    assert_equal 1, offset.offset_shift_c.length
-    assert_equal 1, offset.offset_shift_d.length
+    assert_equal "4641", offset.squared_last_four
   end
 end
