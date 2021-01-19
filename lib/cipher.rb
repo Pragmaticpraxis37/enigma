471038c4a7e8d7d61f29c568e53cdc81cbd006fb
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-18 19:09:50 -0700

    Remove commented out code

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,4 +1,8 @@
+require './lib/shiftable'
+
 class Cipher
+  include Shiftable
+
   attr_reader :message,
               :key,
               :offset,
@@ -26,43 +30,10 @@ class Cipher
     create_shift_sets
   end
 
-  def create_sub_keys
-    ord_letter = 97
-
-    @key.each_cons(2).reduce({}) do |sub_keys, cons_nums|
-      sub_keys[ord_letter.chr] = cons_nums.join.to_i
-      ord_letter += 1
-      sub_keys
-    end
-  end
-
-  def create_sub_offsets
-    ord_letter = 97
-
-    @offset.reduce({}) do |sub_offsets, num|
-      sub_offsets[ord_letter.chr] = num.to_i
-      ord_letter += 1
-      sub_offsets
-    end
-  end
-
-  def create_shifts
-    create_sub_keys.each do |key_letter, key_value|
-      create_sub_offsets.each do |offset_letter, offset_value|
-        if key_letter == offset_letter
-          @shifts[key_letter] = key_value + offset_value
-        end
-      end
-    end
-  end
-
   def create_unciphered_index_collection
     @message.reduce([]) do |acc, letter|
       acc << original_set.index(letter)
     end
-    # @message.each do |letter|
-    #   @unciphered_index_collection << original_set.index(letter)
-    # end
   end
 
   def create_shift_sets
