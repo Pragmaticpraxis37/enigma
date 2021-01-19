031d773e263674fe10d75bedf7f0bf10e97601d0
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-18 19:10:04 -0700

    Remove commented out code

--- lib/decipher.rb
+++ lib/decipher.rb
@@ -1,4 +1,8 @@
+require './lib/shiftable'
+
 class Decipher
+  include Shiftable
+
   attr_reader :ciphertext,
               :key,
               :offset,
@@ -24,36 +28,6 @@ class Decipher
     create_shifts
     create_shift_sets
     create_ciphered_index_collection
-  end
-
-  def create_sub_keys
-    ord_letter = 97
-
-    @key.each_cons(2).reduce({}) do |sub_keys, cons_nums|
-      sub_keys[ord_letter.chr] = cons_nums.join.to_i
-      ord_letter += 1
-      sub_keys
-    end
-  end
-
-  def create_sub_offsets
-    ord_letter = 97
-
-    @offset.reduce({}) do |sub_offsets, num|
-      sub_offsets[ord_letter.chr] = num.to_i
-      ord_letter += 1
-      sub_offsets
-    end
-  end
-
-  def create_shifts
-    create_sub_keys.each do |key_letter, key_value|
-      create_sub_offsets.each do |offset_letter, offset_value|
-        if key_letter == offset_letter
-          @shifts[key_letter] = key_value + offset_value
-        end
-      end
-    end
   end
 
   def create_shift_sets
@@ -95,5 +69,4 @@ class Decipher
       @original_set[index]
     end.join
   end
-
 end
